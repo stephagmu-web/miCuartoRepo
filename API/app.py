b805287e6a4c9df7634def92c188b45f57ce03d9
Stephany <sgeralmattuloa@gmail.com>
2026-03-02 21:47:26 +0100

    nuevo proyecto de API creado

---
+++ API/app.py
@@ -0,0 +1,16 @@
+import os
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/superheroesDC")
+def get_superheroes():
+    rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
+    return rows
+
+@app.get("/superheroesMarvel")
+def get_superheroes_marvel():
+    rows = ["Spider-Man", "Iron Man", "Thor", "Hulk", "Black Widow", "Doctor Strange", "Black Panther", "Captain America"]
+    return rows
